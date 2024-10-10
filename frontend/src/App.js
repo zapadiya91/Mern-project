7b706847838423ac32d1f49bf95eae7a9cc352ab
Zapadiya Haresh <142992715+zapadiya91@users.noreply.github.com>
2024-10-10 13:37:16 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./AddForm.css";
+import "./Main.css";
+
+import Main from "./component/Main";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
